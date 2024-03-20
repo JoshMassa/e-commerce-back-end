a91562d3304aff8767c3dec38b2193e5eff0c692
Josh Massa <joshmassa@gmail.com>
2024-03-20 15:47:41 -0400

    synched sequelize to database

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
+
